d688c86099de077c151e605f72c69058d376f7c6
Marius <m.fjeldwold@gmail.com>
2016-06-18 13:14:00 +0300

    Psychedelic hand tracking and x,y coord logging awesomeness

--- program.py
+++ program.py
@@ -7,22 +7,45 @@
  #
  # ###########################################################################/
 
+ # ############################################################################
+ #
+ # Modifications made to code by the awesome group 5 of the 2016 Aalto
+ # university summer school on design interaction.
+ #
+ # This code is for the saturday hackathon
+ #
+ # ###########################################################################/
+
 import thread
 import itertools
 import ctypes
+import random
 
 import pykinect
 from pykinect import nui
 from pykinect.nui import JointId
+from pykinect.nui import SkeletonTrackingState
+from pykinect.nui.structs import TransformSmoothParameters
 
 import pygame
 from pygame.color import THECOLORS
 from pygame.locals import *
 
 KINECTEVENT = pygame.USEREVENT
-DEPTH_WINSIZE = 320,240
+DEPTH_WINSIZE = 800,600
 VIDEO_WINSIZE = 640,480
 pygame.init()
+
+SMOOTH_PARAMS_SMOOTHING = 0.7
+SMOOTH_PARAMS_CORRECTION = 0.4
+SMOOTH_PARAMS_PREDICTION = 0.7
+SMOOTH_PARAMS_JITTER_RADIUS = 0.1
+SMOOTH_PARAMS_MAX_DEVIATION_RADIUS = 0.1
+SMOOTH_PARAMS = TransformSmoothParameters(SMOOTH_PARAMS_SMOOTHING,
+                                          SMOOTH_PARAMS_CORRECTION,
+                                          SMOOTH_PARAMS_PREDICTION,
+                                          SMOOTH_PARAMS_JITTER_RADIUS,
+                                          SMOOTH_PARAMS_MAX_DEVIATION_RADIUS)
 
 SKELETON_COLORS = [THECOLORS["red"],
                    THECOLORS["blue"],
@@ -59,18 +82,16 @@ SPINE = (JointId.HipCenter,
 
 skeleton_to_depth_image = nui.SkeletonEngine.skeleton_to_depth_image
 
-def draw_skeleton_data(pSkelton, index, positions, width = 4):
-    start = pSkelton.SkeletonPositions[positions[0]]
+movement = open('log.txt','w')
 
-    for position in itertools.islice(positions, 1, None):
-        next = pSkelton.SkeletonPositions[position.value]
+def draw_skeleton_data(pSkelton, index, position, width = 2):
 
-        curstart = skeleton_to_depth_image(start, dispInfo.current_w, dispInfo.current_h)
-        curend = skeleton_to_depth_image(next, dispInfo.current_w, dispInfo.current_h)
+    handPos = skeleton_to_depth_image(pSkelton.SkeletonPositions[position],dispInfo.current_w,dispInfo.current_w)
 
-        pygame.draw.line(screen, SKELETON_COLORS[index], curstart, curend, width)
+    log_hand_pos(int(handPos[0]),int(handPos[1]),position)
 
-        start = next
+    pygame.draw.circle(screen, SKELETON_COLORS[random.randint(0,len(SKELETON_COLORS)-1)], (int(handPos[0]),int(handPos[1])), 10, 0)
+
 
 # recipe to get address of surface: http://archives.seul.org/pygame/users/Apr-2008/msg00218.html
 if hasattr(ctypes.pythonapi, 'Py_InitModule4'):
@@ -98,17 +119,10 @@ def surface_to_array(surface):
 
 def draw_skeletons(skeletons):
     for index, data in enumerate(skeletons):
-        # draw the Head
-        HeadPos = skeleton_to_depth_image(data.SkeletonPositions[JointId.Head], dispInfo.current_w, dispInfo.current_h)
-        draw_skeleton_data(data, index, SPINE, 10)
-        pygame.draw.circle(screen, SKELETON_COLORS[index], (int(HeadPos[0]), int(HeadPos[1])), 20, 0)
 
-        # drawing the limbs
-        draw_skeleton_data(data, index, LEFT_ARM)
-        draw_skeleton_data(data, index, RIGHT_ARM)
-        draw_skeleton_data(data, index, LEFT_LEG)
-        draw_skeleton_data(data, index, RIGHT_LEG)
-
+        # Draw left and right hand
+        draw_skeleton_data(data, index, JointId.HandRight)
+        draw_skeleton_data(data, index, JointId.HandLeft)
 
 def depth_frame_ready(frame):
     if video_display:
@@ -116,7 +130,6 @@ def depth_frame_ready(frame):
 
     with screen_lock:
         address = surface_to_array(screen)
-        #ctypes.memmove(address, frame.image.contents.bits, len(address))
         del address
         if skeletons is not None and draw_skeleton:
             draw_skeletons(skeletons)
@@ -129,21 +142,26 @@ def video_frame_ready(frame):
 
     with screen_lock:
         address = surface_to_array(screen)
-        #ctypes.memmove(address, frame.image.contents.bits, len(address))
         del address
         if skeletons is not None and draw_skeleton:
             draw_skeletons(skeletons)
         pygame.display.update()
 
+def log_hand_pos(hand_x, hand_y, position):
+    # We have some frames where we don't get the x,y coords, we'll just ignore those
+    if(hand_x != 0 and hand_y != 0):
+        movement.write("Hand: " + str(position) + " X-coord: " + str(hand_x) + " Y-coord: " + str(hand_y))
+        movement.write("\n")
+
 if __name__ == '__main__':
-    full_screen = False
+    full_screen = True
     draw_skeleton = True
     video_display = False
 
     screen_lock = thread.allocate()
 
     screen = pygame.display.set_mode(DEPTH_WINSIZE,0,16)
-    pygame.display.set_caption('Python Kinect Demo')
+    pygame.display.set_caption('Super Awesome Light Body Control Thing')
     skeletons = None
     screen.fill(THECOLORS["black"])
 
@@ -164,6 +182,8 @@ if __name__ == '__main__':
     kinect.video_stream.open(nui.ImageStreamType.Video, 2, nui.ImageResolution.Resolution640x480, nui.ImageType.Color)
     kinect.depth_stream.open(nui.ImageStreamType.Depth, 2, nui.ImageResolution.Resolution320x240, nui.ImageType.Depth)
 
+    print len(SKELETON_COLORS)
+
     print('Controls: ')
     print('     d - Switch to depth view')
     print('     v - Switch to video view')
@@ -181,6 +201,7 @@ if __name__ == '__main__':
             done = True
             break
         elif e.type == KINECTEVENT:
+            #kinect._nui.NuiTransformSmooth(e.skeletom_frame,SMOOTH_PARAMS)
             skeletons = e.skeletons
             if draw_skeleton:
                 draw_skeletons(skeletons)
@@ -205,3 +226,5 @@ if __name__ == '__main__':
                 kinect.camera.elevation_angle = kinect.camera.elevation_angle - 2
             elif e.key == K_x:
                 kinect.camera.elevation_angle = 2
+
+    movement.close()
